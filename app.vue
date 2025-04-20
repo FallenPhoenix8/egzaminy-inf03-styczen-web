086b3ef8c57bdbfa5bce3f9d64272943d41c4db2
FallenPhoenix8 <tplayxyz8@gmail.com>
2025-04-20 16:56:35 +0200

    Create footer

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { ExamDetails, Exam } from "./types/Exam"
+import { MailOpen, Github } from "lucide-vue-next"
 
 const isSearchDialogOpen = ref(false)
 
@@ -43,5 +44,34 @@ watch(
   <main class="min-h-screen py-1">
     <NuxtPage />
   </main>
-  <footer></footer>
+  <footer class="text-muted-foreground p-2">
+    <h3>
+      &copy; {{ new Date().getFullYear() }} Łukasz Kwiecień - wygląd i działanie
+      strony
+    </h3>
+
+    <h4 class="text-lg text-semibold">Skontaktuj się ze mną:</h4>
+
+    <ul class="space-y-2">
+      <li>
+        <a href="mailto:lukw8@proton.me">
+          <UIButton><MailOpen /> Email</UIButton>
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/FallenPhoenix8" target="_blank">
+          <UIButton
+            class="bg-[#151b23] hover:bg-[#151b23]/50 focus:bg-[#151b23]/50 text-white"
+            ><Github /> GitHub</UIButton
+          >
+        </a>
+      </li>
+    </ul>
+    <hr />
+    <div>
+      &copy;
+      <a href="https://cke.gov.pl/egzamin-zawodowy/">CKE</a>
+      - arkusze egzaminacyjne
+    </div>
+  </footer>
 </template>
